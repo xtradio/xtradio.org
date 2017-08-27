6cd0ae3c06aa6a629aaace0ca4432b2f7dd260a0
Akos Veres <veres@akos.me>
2017-08-27 23:38:52 +0100

    js file cleanup and update for upcoming api change

--- js/xt.js
+++ js/xt.js
@@ -3,12 +3,13 @@ var total = "0";
 var oldTitle = "";
 var length = "";
 function duration() {
-var percent = reload/total*100;
-var eqLineColor = "rgb(0, 0, 0)";
-}
+    var percent = reload/total*100;
+    }
+
+function fmtMSS(s){return(s-(s%=60))/60+(9<s?':':':0')+s}
 
 function nowplaying() {
-    $.getJSON('https://xtradio.org/API/np.php', function (data) {
+    $.getJSON('https://api.kicsi.puck.in/api', function (data) {
 
         if ( typeof data.song !== 'undefined' || data.song !== oldTitle ) {
             if ( data.song === oldTitle ) { 
@@ -19,53 +20,24 @@ function nowplaying() {
             oldTitle = data.song;
             var artist = data.artist;
             var show = data.show;
-            if ( data.lenght != 'false' || data.lenght !== 'false' ) { 
-    	        length = ' (' + data.lenght + ')'; 
+            if ( data.length != 'false' || data.length !== 'false' ) { 
+		        length = ' (' + fmtMSS(data.length) + ')';
     	    }
     	    else { 
     	        length = ''; 
     	    }
     	if ( typeof data.secs !== 'undefined' ) { total = data.secs; }
     
-    //	if ( data.share !== '' && typeof data.share !== 'undefined' ) { 
-    //	document.getElementById("share").innerHTML = '<a href="' + data.share + '" target="_blank"><img src="//blog.xtradio.org/wp-content/themes/xtradio/images/social_soundcloud.png" height="40" width="40" title="' + data.artist + ' - ' + data.song + ' on Soundcloud" ></a>'
-    //	$("#share").show();
-    //	}
-    //	else {
-    //	$("#share").hide();
-    //	}
-    
-    	if ( typeof data.lenght !== 'undefined') {
-    	    document.getElementById("title").innerHTML = title + ' (' + data.lenght + ')';
+    	if ( typeof data.length !== 'undefined') {
+    	    document.getElementById("title").innerHTML = title + ' (' + data.length + ')';
                 var ongoing = data.secs - data.remaining
-    //    $("#progressbar").val(ongoing);
-    //    $("#progressbar").attr("max", data.secs);
-    //                        var progressbar = $('#progressbar'),
-    //                                max = data.secs,
-    //                                time = 1000,
-    //                        value = progressbar.val();
-    //
-    //
-    //                    var loading = function() {
-    //                        value += 1;
-    //                        addValue = progressbar.val(value);
-    //
-    //                        if (value == max) {
-    //                            clearInterval(animate);
-    //                        }
-    //                    };
-    //
-    //                    var animate = setInterval(function() {
-    //                        loading();
-    //                    }, time);
-    
     
     	}
     	else {
     	    document.getElementById("title").innerHTML = title;
     	}
 
-	var img = 'https:' + data.image + '?callback=?';
+	var img = data.image + '?callback=?';
     	if ( typeof data.remaining !== 'undefined' ) {
     	    reload = data.remaining * 1000 + 1000;
     	    setTimeout( "nowplaying()", reload )
@@ -108,12 +80,9 @@ if ( typeof durVar === 'undefined' ) {
 
 }
 
-// var npr = setInterval(function(){nowplaying()},reload);
 
 window.onload = function(){
 nowplaying();
-//$("#metadata").toggle();
-//$("#player").toggle();
 setTimeout( "nowplaying()", reload );
 
 $(window).keypress(function(e) {
