ea70a16fbf3a6f4dc4e4b501860f0265d035de7b
Akos Veres <veres@akos.me>
2018-02-02 20:20:00 +0000

    Add production env to tests

--- tests/cibuild.sh
+++ tests/cibuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec jekyll build --source /home/travis/build/xtradio/xtradio.org/jekyll --destination /home/travis/_site
+JEKYLL_ENV=production bundle exec jekyll build --source /home/travis/build/xtradio/xtradio.org/jekyll --destination /home/travis/_site
 bundle exec htmlproofer /home/travis/_site --disable-external
