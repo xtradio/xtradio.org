1014defe287d0225f6af9ec3d635c146778d0298
Elton Renda <elton@ebrdev.co.za>
2020-04-07 20:00:22 +0200

    fix: live show remaining

--- src/App.svelte
+++ src/App.svelte
@@ -39,13 +39,12 @@
 
       // Is live show?
       const live  = show === 'live' || (length === 0 && remaining === 10);
-      // If live show only refresh every 15 seconds
-      remaining = live ? 0 : remaining - Math.ceil((Date.now() - start) / 1000);
+      remaining -= Math.ceil((Date.now() - start) / 1000);
 
       // Refresh information timer
-      setTimeout(refresh, (remaining || 15) * 1000);
+      setTimeout(refresh, remaining * 1000);
 
-      tracks.current  = {...data.current, remaining};
+      tracks.current  = {...data.current, remaining: live ? 0 : remaining};
       tracks.previous = data.previous;
     }, false);
     loader.src = `${data.current.image}?${start}`;
