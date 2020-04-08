235b16a6bf7346912ab5e7707df94eae7bdab5f7
Elton Renda <elton@ebrdev.co.za>
2020-04-08 20:09:14 +0200

    fix: pwa init

---
+++ .workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  importScripts: [],
+  globDirectory: 'dist',
+  globPatterns: [
+    '**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'
+  ],
+  skipWaiting: true
+};
